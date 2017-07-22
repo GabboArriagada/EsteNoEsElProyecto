b1aa0901a53848ef8b66577606a99a99173f6021
Gabbo <gabriel.arriagadag@utem.cl>
2017-07-22 18:59:11 -0400

    cambios en el estilo

--- db/schema.rb
+++ db/schema.rb
@@ -134,7 +134,11 @@ ActiveRecord::Schema.define(version: 20170721210316) do
     t.integer  "telefono"
     t.integer  "fecha_ingreso"
     t.string   "sexo"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string   "last_name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
