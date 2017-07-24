34196b0d596cfec6f03f04a8d8c99f917ba7edd1
Gabbo <gabriel.arriagadag@utem.cl>
2017-07-24 18:18:21 -0400

    Confirmación de cuenta y vistas

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -113,8 +113,8 @@ Devise.setup do |config|
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
 
-  # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  #Send a notification email when the user's password is changed.
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -122,7 +122,7 @@ Devise.setup do |config|
   # able to access the website for two days without confirming their account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming their account.
-  # config.allow_unconfirmed_access_for = 2.days
+  config.allow_unconfirmed_access_for = 0.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721210316) do
+ActiveRecord::Schema.define(version: 20170724205300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20170721210316) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "last_name"
+    t.string   "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
