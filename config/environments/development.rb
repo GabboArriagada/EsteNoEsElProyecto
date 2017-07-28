39355eb3f8d140f9b9ae7475cf679123bc0c1080
Gabbo <gabriel.arriagadag@utem.cl>
2017-07-27 23:29:41 -0400

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -55,12 +55,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
+   address: 'smtp.gmail.com',
+   port: 587,
+   domain: 'gmail.com',
+   user_name: ENV['gmail_username'],
+   password: ENV['gmail_password'],
+   authentication: 'plain',
+   #:enable_starttls_auto => true
   }
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,16 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = {:host => "34.205.28.161"}
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
+   address: 'smtp.gmail.com',
+   port: 587,
+   domain: 'gmail.com',
+   user_name: ENV['gmail_username'],
+   password: ENV['gmail_password'],
+   authentication: 'plain',
+   #:enable_starttls_auto => true
   }
 
 end
