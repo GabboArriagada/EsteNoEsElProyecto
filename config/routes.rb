61caa22960430712c1051073bcc2c3f884fe7753
Unknown <gabriel.arriagadag@utem.cl>
2017-11-14 19:52:44 -0300

    arreglo de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,52 @@
 Rails.application.routes.draw do
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :posts
+mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+resources :posts
+resources :asignaturas
+resources :cursos
+resources :contacts, only: [:new, :create]
+get "archivos/subir_archivos"
+post "archivos/subir_archivos"
+get "archivos/listar_archivos"
+post "archivos/borrar_archivos"
+get "archivos/guardar_comentarios"
+post "archivos/guardar_comentarios"
+
+get 'foro/index'
+resources :asignaturas do
+  resources :notas
+end
+resources :questions do
+resources :answers
+end
+resources :events
+
+get 'calendario/index'
+# seccion alumno
+namespace :alumno do
+  resources :notas
+  match 'notas/show_nota' => 'notas#show_nota', via: :get
   resources :asignaturas
+end
+
+#seccion profesor
+namespace :profesor do
+  resources :notas
   resources :cursos
-  resources :contacts, only: [:new, :create]
-  get "archivos/subir_archivos"
-  post "archivos/subir_archivos"
-  get "archivos/listar_archivos"
-  post "archivos/borrar_archivos"
-  get "archivos/guardar_comentarios"
-  post "archivos/guardar_comentarios"
-
-  get 'foro/index'
   resources :asignaturas do
-    resources :notas
+    member do
+      get :notas
+    end
   end
-  resources :questions do
-  resources :answers
-  end
-  resources :events
-
-  get 'calendario/index'
-  # seccion alumno
-  namespace :alumno do
-    resources :notas
-    match 'notas/show_nota' => 'notas#show_nota', via: :get
-    resources :asignaturas
-  end
-
-  #seccion profesor
-  namespace :profesor do
-    resources :notas
-    resources :asistencia
-      resources :asignaturas do
-        member do
-          get :notas
-        end
-      end
-    resources :posts
-  end
+  resources :posts
+end
 
 
 
-  devise_for :users, controllers: { registrations: 'registrations' }
-  root 'welcome#index'
-  get 'welcome/index'
-  get "inicio", to:"welcome#index"
+devise_for :users, controllers: { registrations: 'registrations' }
+root 'welcome#index'
+get 'welcome/index'
+get "inicio", to:"welcome#index"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
