f9c16f9679fb858416d6d47be3c1e7c04cbdff33
Gabriel <gabriel@felitech.com>
2019-06-26 17:51:34 -0400

    cambios de vistas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def bootstrap_icon_for flash_type
+    { success: "ok-sign", error: "remove-sign", alert: "warning-sign", notice: "exclamation-sign" }[flash_type] || 'info-sign'
+  end
 end
--- db/migrate/20171209220144_add_unaccent_extension.rb
+++
@@ -1,8 +0,0 @@
-class AddUnaccentExtension < ActiveRecord::Migration[5.0]
-  def up
-    execute "create extension unaccent"
-  end
-  def down
-    execute "drop extension unaccent"
-  end
-end
