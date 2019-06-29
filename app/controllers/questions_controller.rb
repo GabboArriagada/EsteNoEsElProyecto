22c8bbe32adccbdf77ff5b453e50b7004ed38bb2
Gabriel <gabriel@felitech.com>
2019-06-29 01:21:12 -0400

    borrar editar preguntas respuestas

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -74,6 +74,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:content)
+      params.require(:question).permit(:content, :temas_attributes => [:id, :tema_id])
     end
 end
