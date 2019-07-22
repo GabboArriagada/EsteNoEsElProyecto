edf91a34f3ef505201def3b7a4a375368f152ec9
Gabriel <gabriel@felitech.com>
2019-07-22 15:42:32 -0400

    script busqueda global

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,25 @@
 //= require bootstrap.file-input
 //= require bootstrap-datetimepicker
 //= require moment/es.js
+function myFunction() {
+  var input,
+    filter,
+    tabla,
+    tr,
+    td,
+    i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
